47ed17946fe4ce62a894a8b1a5bfd3c7907fa7da
Asoji <asoji-git@hiitsdevin.mozmail.com>
2022-10-13 21:21:13 -0700

    🦀 Crab in the cooooode!

--- src/main/kotlin/one/devos/vrbadge/Application.kt
+++ src/main/kotlin/one/devos/vrbadge/Application.kt
@@ -45,5 +45,6 @@ fun main() {
         configureSerialization()
         configureSockets()
         configureRouting()
+        crab()
     }.start(wait = true)
 }
---
+++ src/main/kotlin/one/devos/vrbadge/plugins/Crab.kt
@@ -0,0 +1,18 @@
+package one.devos.vrbadge.plugins
+
+import io.ktor.server.application.*
+
+fun Application.crab() {
+    /**
+     * This is an extremely vital class. This class is crucial for the correct functionality of
+     * VR Badge Backend. This is extremely important, and cannot be removed under any circumstances.
+     * This class single-handedly holds together the Engine. Without this class, everything
+     * falls apart. This class is the reason humanity has not yet gone extinct. This class is
+     * the only thing holding together the remains of the time-space continuum. Without this incredibly
+     * crucial class, everything ever will fail catastrophically. In the event this class is removed,
+     * expect you to go next, followed by the rest of humanity, and finally all of reality.
+     * Once again, DO NOT REMOVE THIS CLASS.
+     *
+     * 🦀
+     */
+}
